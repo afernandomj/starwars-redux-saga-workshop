c92fe9925cee350cf11e92bcf59a7adb244c7623
Andres Fernando Moreno <andres.moreno@pluto.tv>
2022-05-20 14:39:24 -0500

    setup redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 
 import reducer from '../reducers';
@@ -6,7 +7,11 @@ import sagas from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
-export const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+// export const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+export const store = configureStore({
+    reducer,
+    middleware: [sagaMiddleware],
+})
 
 sagaMiddleware.run(sagas);
 
